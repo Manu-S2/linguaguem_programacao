7fa534fba3d822a16ed0681cd05330f8b6e1374b
Manu <86742231+Manu-S2@users.noreply.github.com>
2023-04-18 07:59:15 -0300

    Add files via upload

---
+++ exercicio_01.py
@@ -0,0 +1,16 @@
+class escola ():
+
+    def funcao ( self, nota, prova1, prova2 , trabalho, aluno):
+
+        self.aluno = aluno
+        self.prova1 = prova1
+        self.prova2 = prova2
+        self.trabalho = trabalho
+
+   
+
+
+
+
+
+
---
+++ exercicio_02.py
@@ -0,0 +1,2 @@
+from exercicio_01 import escola
+jorge = funcao (1, 7.9, 5.6, 5.8,"Jorge")
